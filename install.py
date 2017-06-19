bdd8ba544b9ab7c16333e4199cf8e55da5c6ae36
Glenn McKechnie <glenn.mckechnie@users.noreply.github.com>
2017-06-20 09:41:22 +1000

    more housekeeping, mysql -> sql changes

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
-# installer for MySQLBackup
+# installer for SqlBackup
 # Copyright 2016 Matthew Wall
 # Co-opted by Glenn McKechnie 2017
 # Distributed under the terms of the GNU Public License (GPLv3)
@@ -6,23 +6,23 @@
 from setup import ExtensionInstaller
 
 def loader():
-    return MySQLBackupInstaller()
+    return SqlBackupInstaller()
 
-class MySQLBackupInstaller(ExtensionInstaller):
+class SqlBackupInstaller(ExtensionInstaller):
     def __init__(self):
-        super(MySQLBackupInstaller, self).__init__(
+        super(SqlBackupInstaller, self).__init__(
             version="0.2",
-            name='MySQLBackup',
-            description='Use mysqldump to create Partial backups of the MySQL weewx database',
+            name='SqlBackup',
+            description='Use mysqldump or .dump to create Partial backups of the weewx databases',
             author="Glenn McKechnie",
             author_email="glenn.mckechnie@gmail.com",
             config={
                 'StdReport': {
-                    'MySQLBackup': {
-                        'skin': 'mysqlbackup'}}},
-            files=[('bin/user', ['bin/user/mysqlbackup.py']),
-                   ('skins/mysqlbackup', ['skins/mysqlbackup/skin.conf',
-                    'skins/mysqlbackup/mysqlbackup.html.tmpl',
-                    'skins/mysqlbackup/mysqlbackup.inc'])
+                    'SqlBackup': {
+                        'skin': 'sqlbackup'}}},
+            files=[('bin/user', ['bin/user/sqlbackup.py']),
+                   ('skins/sqlbackup', ['skins/sqlbackup/skin.conf',
+                    'skins/sqlbackup/sqlbackup.html.tmpl',
+                    'skins/sqlbackup/sqlbackup.inc'])
                   ]
         )
